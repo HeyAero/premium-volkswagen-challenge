6721fd6cf716c6f95c7707318c93e706e68941c6
Aaron <aeroyts@icloud.com>
2020-11-27 18:27:36 +0000

    Comparison and features section added
    Added comparison feature and features section. Features will disappear on mobile until I add a re-worked section for small devices.

--- js/jquery.js
+++ js/jquery.js
@@ -11,3 +11,51 @@ function setSticky() {
     navbar.classList.remove('sticky-top');
   }
 }
+
+$('#feature-button-key').click(function () {
+  $('#feature-button-key').addClass('feature-active');
+  $('#feature-button-interior').removeClass('feature-active');
+  $('#feature-button-exterior').removeClass('feature-active');
+  $('#feature-button-saftey').removeClass('feature-active');
+
+  $('#feature-key').removeClass('d-none');
+  $('#feature-interior').addClass('d-none');
+  $('#feature-exterior').addClass('d-none');
+  $('#feature-saftey').addClass('d-none');
+});
+
+$('#feature-button-interior').click(function () {
+  $('#feature-button-key').removeClass('feature-active');
+  $('#feature-button-interior').addClass('feature-active');
+  $('#feature-button-exterior').removeClass('feature-active');
+  $('#feature-button-saftey').removeClass('feature-active');
+
+  $('#feature-interior').removeClass('d-none');
+  $('#feature-key').addClass('d-none');
+  $('#feature-exterior').addClass('d-none');
+  $('#feature-saftey').addClass('d-none');
+});
+
+$('#feature-button-exterior').click(function () {
+  $('#feature-button-key').removeClass('feature-active');
+  $('#feature-button-interior').removeClass('feature-active');
+  $('#feature-button-exterior').addClass('feature-active');
+  $('#feature-button-saftey').removeClass('feature-active');
+
+  $('#feature-exterior').removeClass('d-none');
+  $('#feature-key').addClass('d-none');
+  $('#feature-interior').addClass('d-none');
+  $('#feature-saftey').addClass('d-none');
+});
+
+$('#feature-button-saftey').click(function () {
+  $('#feature-button-key').removeClass('feature-active');
+  $('#feature-button-interior').removeClass('feature-active');
+  $('#feature-button-exterior').removeClass('feature-active');
+  $('#feature-button-saftey').addClass('feature-active');
+
+  $('#feature-saftey').removeClass('d-none');
+  $('#feature-key').addClass('d-none');
+  $('#feature-interior').addClass('d-none');
+  $('#feature-exterior').addClass('d-none');
+});
